c2dfc7c78e10369226e0623d8117c79465875e33
Sean Luckett <whitecrane007@gmail.com>
2018-03-09 12:06:20 -0700

    grocery list: sets up project with material-ui css library

--- grocery_list/src/index.js
+++ grocery_list/src/index.js
@@ -1,50 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+
+import App from './components/App';
 import {createStore} from 'redux';
 import {groceryListApp} from "./reducers";
-import {
-  addItem, purchaseItem, setCategoryFilter, setPurchaseFilter,
-  setSortFilter
-} from "./actions";
+
+import registerServiceWorker from './registerServiceWorker';
 
 let store = createStore(groceryListApp);
-let unsubscribe = store.subscribe(() => {
-  console.log(store.getState());
-});
 
-console.log('Initial state: ', store.getState());
-
-store.dispatch(addItem({
-  name: 'bananas',
-  description: 'Squishy things in yellow suits',
-  cost: null,
-  category: null,
-  purchased: false,
-}));
-
-store.dispatch(addItem({
-  name: 'apples',
-  description: 'How you like dem?',
-  cost: null,
-  category: null,
-  purchased: false,
-}));
-
-store.dispatch(purchaseItem(1));
-
-store.dispatch(setCategoryFilter('SHOW_FRUIT'));
-
-store.dispatch(setPurchaseFilter('PURCHASED'));
-store.dispatch(setPurchaseFilter('NOT_PURCHASED'));
-
-store.dispatch(setSortFilter('DESCRIPTION'));
-store.dispatch(setSortFilter('NAME'));
-
-unsubscribe();
-
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
